15d39efe29fc5006a9edcfe1946589aeb29f1e5e
alifandi <102402115+al1fandi@users.noreply.github.com>
2023-06-05 14:49:37 +0700

    Update Bellabeat Case Study with R.R

--- Bellabeat Case Study with R.R
+++ Bellabeat Case Study with R.R
@@ -1,14 +1,14 @@
 # Case Study 1: How Can a Wellness Technology Company Play It Smart?
 
 # Project Overview
-* Bellabeat is a small, successful company that has the potential to become a larger company in the global smart device market. 
-* There is a need to analyze smart device fitness data to unlock new growth opportunities for the company. 
-* The analysis focuses on one of Bellabeats products. 
-* The smart device data analysis is useful to gain insights into how consumers use Bellabeats smart devices and will further guide the marketing strategy for the company. 
-* The results of the analysis will be presented to the executive team along with recommendations for Bellabeats marketing strategy.
-* Smart device usage data is viewed based on daily activity, calories, intensity, sleep time, and weight.
-* This analysis uses 5 selected tables with a total of 45,618 queries from the entire database of 18 tables.
-* The entire analysis using R programming language
+- Bellabeat is a small, successful company that has the potential to become a larger company in the global smart device market. 
+- There is a need to analyze smart device fitness data to unlock new growth opportunities for the company. 
+- The analysis focuses on one of Bellabeats products. 
+- The smart device data analysis is useful to gain insights into how consumers use Bellabeats smart devices and will further guide the marketing strategy for the company. 
+- The results of the analysis will be presented to the executive team along with recommendations for Bellabeats marketing strategy.
+- Smart device usage data is viewed based on daily activity, calories, intensity, sleep time, and weight.
+- This analysis uses 5 selected tables with a total of 45,618 queries from the entire database of 18 tables.
+- The entire analysis using R programming language
 
 
 # Question for the analysis
